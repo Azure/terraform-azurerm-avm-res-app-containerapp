e0a95a4d6418dba576f8a20c12885517ad49e61c
lonegunmanb <lonegunmanb@hotmail.com>
2024-11-22 07:31:44 +0000

    fix example

--- examples/auth_config/main.tf
+++ examples/auth_config/main.tf
@@ -1,19 +1,17 @@
-resource "random_id" "rg_name" {
-  byte_length = 8
-}
-
-resource "random_id" "container_name" {
-  byte_length = 4
+resource "random_string" "this" {
+  length  = 8
+  special = false
+  upper   = false
 }
 
 resource "azurerm_resource_group" "test" {
   location = var.location
-  name     = "example-container-app-${random_id.rg_name.hex}"
+  name     = "example-container-app-${random_string.this.result}"
 }
 
 resource "azurerm_container_app_environment" "example" {
   location            = azurerm_resource_group.test.location
-  name                = random_id.rg_name.hex
+  name                = random_string.this.result
   resource_group_name = azurerm_resource_group.test.name
 }
 
@@ -26,7 +24,7 @@ module "app" {
   template = {
     containers = [
       {
-        name   = random_id.container_name.hex
+        name   = random_string.this.result
         memory = "0.5Gi"
         cpu    = 0.25
         image  = "jackofallops/azure-containerapps-python-acctest:v0.0.1"
