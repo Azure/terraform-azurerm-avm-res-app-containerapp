11346b06dc6da385f6ebd42a8dc03c15582c9958
kewalaka <kewalaka@gmail.com>
2025-06-29 06:26:31 +1200

    fix: avmfix

--- main.interfaces.tf
+++ main.interfaces.tf
@@ -3,26 +3,26 @@ module "avm_interfaces" {
   version = "0.2.0"
 
   lock                             = var.lock
-  role_assignments                 = var.role_assignments
   role_assignment_definition_scope = "/subscriptions/${data.azapi_client_config.current.subscription_id}"
-
+  role_assignments                 = var.role_assignments
 }
 
 resource "azapi_resource" "role_assignments" {
   for_each = module.avm_interfaces.role_assignments_azapi
 
-  type      = each.value.type
-  body      = each.value.body
   name      = each.value.name
   parent_id = azapi_resource.container_app.id
+  type      = each.value.type
+  body      = each.value.body
 }
 
 resource "azapi_resource" "lock" {
-  count     = var.lock != null ? 1 : 0
-  type      = module.avm_interfaces.lock_azapi.type
-  body      = module.avm_interfaces.lock_azapi.body
+  count = var.lock != null ? 1 : 0
+
   name      = module.avm_interfaces.lock_azapi.name != null ? module.avm_interfaces.lock_azapi.name : "lock-${azapi_resource.container_app.name}"
   parent_id = azapi_resource.container_app.id
+  type      = module.avm_interfaces.lock_azapi.type
+  body      = module.avm_interfaces.lock_azapi.body
 }
 
 moved {
