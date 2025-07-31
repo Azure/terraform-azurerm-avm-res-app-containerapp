40f0da560bf022410fd183b6cda0fe6f643d321f
Dmytro Bondar <git@bonddim.dev>
2025-07-31 14:45:36 +0200

    Bump Azure/avm-utl-interfaces/azure to v0.5.0

--- main.interfaces.tf
+++ main.interfaces.tf
@@ -1,6 +1,6 @@
 module "avm_interfaces" {
   source  = "Azure/avm-utl-interfaces/azure"
-  version = "0.2.0"
+  version = "0.5.0"
 
   enable_telemetry = var.enable_telemetry
   lock             = var.lock
