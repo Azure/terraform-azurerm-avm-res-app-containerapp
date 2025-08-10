3c4b1e9ea17e430530b70766341cfae76f9d7323
lonegunmanb <lonegunmanb@hotmail.com>
2025-08-10 23:36:50 +0800

    Fix role assignments scope and names (#94) (#97)
    * Fix role assignments and lock scopes
    
    * Bump Azure/avm-utl-interfaces/azure to v0.5.0
    
    * Use random_uuid for role assignments
    
    Co-authored-by: Dmytro Bondar <git@bonddim.dev>

--- main.interfaces.tf
+++ main.interfaces.tf
@@ -1,6 +1,6 @@
 module "avm_interfaces" {
   source  = "Azure/avm-utl-interfaces/azure"
-  version = "0.2.0"
+  version = "0.5.0"
 
   enable_telemetry = var.enable_telemetry
   lock             = var.lock
@@ -8,15 +8,16 @@ module "avm_interfaces" {
     system_assigned            = var.managed_identities.system_assigned
     user_assigned_resource_ids = var.managed_identities.user_assigned_resource_ids
   }
-  role_assignment_definition_scope = azapi_resource.container_app.id
-  role_assignments                 = var.role_assignments
+  role_assignment_definition_scope     = azapi_resource.container_app.id
+  role_assignment_name_use_random_uuid = true
+  role_assignments                     = var.role_assignments
 }
 
 resource "azapi_resource" "role_assignments" {
   for_each = module.avm_interfaces.role_assignments_azapi
 
   name           = each.value.name
-  parent_id      = local.resource_group_id
+  parent_id      = azapi_resource.container_app.id
   type           = each.value.type
   body           = each.value.body
   create_headers = var.enable_telemetry ? { "User-Agent" : local.avm_azapi_header } : null
@@ -29,7 +30,7 @@ resource "azapi_resource" "lock" {
   count = var.lock != null ? 1 : 0
 
   name           = module.avm_interfaces.lock_azapi.name != null ? module.avm_interfaces.lock_azapi.name : "lock-${azapi_resource.container_app.name}"
-  parent_id      = local.resource_group_id
+  parent_id      = azapi_resource.container_app.id
   type           = module.avm_interfaces.lock_azapi.type
   body           = module.avm_interfaces.lock_azapi.body
   create_headers = var.enable_telemetry ? { "User-Agent" : local.avm_azapi_header } : null
