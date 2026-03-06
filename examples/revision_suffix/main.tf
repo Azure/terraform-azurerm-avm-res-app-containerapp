0072d0615c8548eadad280f27f09856cbda22d13
lonegunman <lonegunmanb@hotmail.com>
2026-03-06 14:28:15 +0800

    fix lint issue

--- examples/revision_suffix/main.tf
+++ examples/revision_suffix/main.tf
@@ -23,8 +23,6 @@ module "container_app" {
   container_app_environment_resource_id = azurerm_container_app_environment.example.id
   name                                  = "app-${random_id.container_name.hex}"
   resource_group_name                   = azurerm_resource_group.test.name
-  revision_mode                         = "Multiple"
-  enable_telemetry                      = false
   template = {
     revision_suffix = var.revision_suffix
     containers = [
@@ -36,6 +34,7 @@ module "container_app" {
       },
     ]
   }
+  enable_telemetry = false
   ingress = {
     allow_insecure_connections = true
     external_enabled           = true
@@ -45,8 +44,19 @@ module "container_app" {
       percentage      = 100
     }]
   }
+  revision_mode = "Multiple"
 
   depends_on = [
     azurerm_resource_group.test,
   ]
+}
+
+# Query the actual resource state from Azure after module applies
+data "azapi_resource" "container_app" {
+  name                   = module.container_app.name
+  parent_id              = azurerm_resource_group.test.id
+  type                   = "Microsoft.App/containerApps@2024-03-01"
+  response_export_values = ["properties.template.revisionSuffix", "properties.latestRevisionName", "properties.latestReadyRevisionName"]
+
+  depends_on = [module.container_app]
 }
